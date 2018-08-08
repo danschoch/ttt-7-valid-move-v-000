6168b83601adab788f59daf0f583728be6e38b18
Daniel Schoch <d.schoch90@gmail.com>
2018-08-08 21:41:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 def valid_move? (board, index)
   valid = nil
-  if position_taken? == false && index.between?(0,8) == true
+  if position_taken? (board, index) == false && index.between?(0,8) == true
     valid = true
   else
     valid = false
